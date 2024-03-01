8c208433f7f500462496a811ffb047884b142d9d
Mariia <mcherkashina77@gmail.com>
2024-03-01 19:51:30 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
-import Login from "./components/Login";
+import Login from "./components/Login/Login";
+import { Routes, Route } from "react-router-dom";
+import Users from "./components/Users/Users";
 
 function App() {
-  return <Login />;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/users" element={<Users />} />
+      <Route path="*" element={<h2>not found</h2>} />
+    </Routes>
+  );
 }
 
 export default App;
